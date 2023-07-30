e567990b851c31125af1cb956acb58b41964baa1
Kayla Kenney <kaylachristinekenney@gmail.com>
2023-07-30 07:28:36 +0300

    completed simple blog

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import React from 'react';
 import Navbar from "./Navbar";
 import Home from "./Home";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from "./Create";
 import BlogDetails from "./BlogDetails";
+import NotFound from './NotFound';
 
 function App() {
 
@@ -21,6 +23,9 @@ function App() {
             </Route>
             <Route path="/blogs/:id">
               <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
             </Route>
           </Switch>
         </div>
